76ced0a9a8613dfe0616303b5f6bf0b54ccb5896
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-12 19:55:37 +0100

    Carga inicial

---
+++ menu.php
@@ -0,0 +1,24 @@
+<NAV id="menuPrincipal">
+	<DIV class="mini">
+		<P>
+			MENÚ
+		</P>
+	</DIV>
+	<DIV>
+		<UL>
+			<A href="http://www.desdechipiona.es" title="Desde Chipiona Web Oficial">
+				<LI class="seleccionMenu">Inicio</LI>
+			</A>
+			<A href="http://www.galeria.desdechipiona.es" title="Galería Fotográfica Desde Chipiona Ciudad">
+				<LI>Galería</LI>
+			</A>
+			<LI>Menú3</LI>
+			<LI>Menú4</LI>
+			<LI>Menú5</LI>
+			<LI>Menú6</LI>
+			<LI>Menú7</LI>
+			<LI>Menú8</LI>
+		</UL>
+	</DIV>
+</NAV>
+
